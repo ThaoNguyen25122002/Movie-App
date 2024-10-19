9c45152dc5c01c1c333063a933199f331dd5b59d
Cong Thao <nlcongthao@gmail.com>
2024-10-19 18:43:26 +0700

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function App() {
   return (
-    <>
-      <p className="text-red-600 text-center font-bold mx-auto">Movie App</p>
-    </>
+    <div>
+      <header className="flex h-14 items-center justify-between bg-slate-950 px-8 text-white">
+        <div className="flex items-center gap-4">
+          <img src="./netflix.png" alt="" className="w-16 sm:w-28" />
+          <a href="">Phim</a>
+          <a href="">Truyền Hình</a>
+        </div>
+        <div>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="cursor-pointer"
+          />
+        </div>
+      </header>
+    </div>
   );
 }
 
